aed21e0366fa1b8bd0f082101dd4c59c050ba448
Ryota <ryoenoki2013@gmail.com>
2021-12-15 15:49:05 +0900

    navbar and main completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,10 @@ import React from "react"
 
 export default function Navbar() {
   return (
-    <h1>This is my navbar</h1>
+    <nav>
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react" className="nav-icon"/>
+      <h3 className="nav-icon-text">ReactFacts</h3>
+      <h4 className="nav-title">React Course - Project 1</h4>
+    </nav>
   )
 }
